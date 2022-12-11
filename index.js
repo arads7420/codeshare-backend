70364cc4bf0bb88438061cb0b53ba74b665008b5
Aradhya Sharma <aradhyasharma750@gmail.com>
2022-12-11 15:43:07 +0530

    init prisma

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+const authRoutes = require('./routes/auth.js')
+const userRoutes = require('./routes/users.js')
+const postRoutes = require('./routes/posts.js')
+const commentRoutes = require('./routes/comments.js')
+const upvoteRoutes = require('./routes/upvotes.js')
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/upvotes", upvoteRoutes)
+
+
 app.listen(5000, () => {
     console.log("Server is listening on Port-5000")
 })
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const {login, register, logout} = require('../controllers/auth')
+
+router.post('login', login)
+router.post('register', register)
+router.post('logout', logout)
+
+
+
+export default router
